c17e4ea169bb17cbe00970ffca9a2767a380dd0f
Saswat <saswatds@gmail.com>
2017-07-28 12:42:16 +0530

    Finalized the core-service construction with mongoose and without Sequelize and RabbitMQ

--- example/index.js
+++ example/index.js
@@ -1,3 +1,55 @@
 const PedalCore = require('../index')
 
-const core = new PedalCore({})
+const config = {
+    database: {
+        dialect: 'mongoose',
+        connectionUrl: 'mongodb://staging:5f78hg54@ds133271.mlab.com:33271/pedal_staging'
+    },
+    queue: false,
+    logger: {
+        consoleLevel: 'info'
+    }
+}
+
+// Use this model file for mongoose
+const model = ({generator}) => {
+    const userSchema = {}
+    generator('Users', userSchema, (schema) => {
+        schema.pre('save', (next) => {
+            // Do something, something
+        })
+    })
+}
+
+// Format of all files
+const api = ({repo, service}, router) => {
+    router.get('/admin', repo.someFunction)
+    router.post('/admin', service.someFunction)
+}
+
+// Repo should return a object with all the functions
+const repo = ({models, logger}) => {
+    const {User, Trips} = models
+
+    const someFunction = () => {
+
+    }
+
+    return Object.create({someFunction})
+}
+
+const service = ({unirest}) => {
+    const someFunction = () => {
+
+    }
+
+    return Object.create({someFunction})
+}
+
+const core = new PedalCore(config)
+core.registerApi(api)
+core.registerModels(model)
+core.registerRepo(repo)
+core.registerService(service)
+core.start(process.env.PORT)
+
--- index.js
+++ index.js
@@ -1,33 +1,142 @@
+const {Express, Database, DI, Queue, Logger} = require('./lib')
+
+
 class PedalCore {
     constructor(config) {
+        // the config has been intialized
         const defaultConfig = {
-            serviceName: 'core-service',
-            port: 3000,
-            database: 'mongodb',
-            queueEnabled: true
+            name: 'core-service',
+            database: {
+                dialect: 'mongoose',
+                connectionUrl: null
+            },
+            express: {
+                morganStandalone: false, // Make morgan to be standalone and work with winston. Prints to stdout with dev
+                morganLevel: 'common',
+                CORSenabled: true,
+                compressionEnabled: true,
+                parseJSON: true,
+                parseURLEncoded: true,
+                parseHTML: false,
+                port: 3000
+            },
+            queue: {
+                dialect: 'rabbot',
+                connectionUrl: null
+            },
+            logger: {
+                consoleLevel: 'info'
+            }
         }
         this._config = Object.assign(defaultConfig, config)
+
+        this.logger = new Logger(this._config.logger)
+        // Lets create the different object based on the config
+        if (this._config.database) this.database = new Database(this._config.database.dialect, this._config.database.connectionUrl)
+        if (this._config.queue) this.queue = new Queue(this._config.queue.dialect, this._config.queue.connectionUrl)
+        if (this._config.express) this.express = new Express(this._config.express, this.logger)
+
+        // Lets initiate the dependency injection procedure
+        this.di = new DI(this)
+
+        this.apiGenerator = null
+        this.modelGenerator = null
+        this.serviceGenerator = null
+        this.queueGenerator = null
+        this.logger.debug('Core has been constructed...')
     }
 
-    registerApi () {
+    registerApi(apiGenerator) {
+        /*
+         *  The api generator is a function that takes dependences as paraments
+         * const apiGenerator = ({router, repo, service}) => {
+         *   router.get('/admin', repo.someFunction)
+         *   router.post('/admin', service.someFunction)
+         *  }
+         * */
+        this.apiGenerator = apiGenerator
+        this.logger.debug('Api Generator has been set')
+    }
+
+    registerRepo(repoGenarator) {
+        // The repo Generator is also one more generator function that takes the dependencies as parameter
+        this.repoGenarator = repoGenarator
+        this.logger.debug('Repo Generator has been set')
 
     }
 
-    registerRepo (repoGenarator) {
-        return repoGenarator()
-    }
-
-    registerService () {
+    registerService(serviceGenerator) {
+        this.serviceGenerator = serviceGenerator
+        this.logger.debug('Service Generator has been set')
 
     }
 
-    registerModel () {
+    registerModels(modelGenerator) {
+        this.modelGenerator = modelGenerator
+        this.logger.debug('Model Generator has been set')
 
     }
 
-    start(port) {
-        if (port) this._config.port = port
-        // Start the callback sequence
+    connectDB() {
+        if (!this.database) return Promise.resolve()
+        this.logger.debug('Connecting to database')
+        this.database.connect()
+    }
+
+    connectQueue() {
+        if (!this.queue) return Promise.resolve()
+        this.logger.debug('Connecting to queue')
+        return this.queue.connect()
+    }
+
+
+    startExpress() {
+        if (!this.express) return Promise.resolve()
+        this.logger.debug('Starting express')
+        return this.express.start()
+    }
+
+    start() {
+        // The is the main orchestrator function
+        if (this.database && this.modelGenerator) {
+            // pre >> prepare all the model and set them at database
+            //TODO: Probally have to convert to an array
+            this.modelGenerator({generator: this.database.modelGenerator()})
+            this.di.registerValue({models: this.database.models})
+        }
+        // pre >> prepare all the exchanges and queues
+        if (this.queue && this.queueGenerator) {
+            this.logger.warn("The queue implementation has not yet been completed")
+            //TODO: Add the queue to the dependency
+        }
+
+        // register all services
+        if (this.serviceGenerator) {
+            // TODO: Bind to the dependency injection procedure
+
+            const service = this.serviceGenerator.call(null, this.di.container.cradle)
+            this.di.registerValue({service})
+        }
+
+        if (this.repoGenarator) {
+            const repo = this.repoGenarator.call(null, this.di.container.cradle)
+            this.di.registerValue({repo})
+        }
+
+        if (this.express && this.apiGenerator) {
+            const bindedGenerator = this.apiGenerator.bind(null, this.di.container.cradle)
+            this.express.registerRoutes(bindedGenerator)
+        }
+        Promise.all([
+            this.connectDB(),
+            this.connectQueue()
+        ]).then(() => {
+            return this.startExpress()
+        }).then(() => {
+            this.logger.info(`${this._config.name} has started ... `)
+        }).catch((err) => {
+            this.logger.error(err)
+        })
     }
 }
 
